3293e7944031e30c9825f868d041e6f9c07aa9db
Cameron Bridges <cameron.bridges@fortiviewholdings.com>
2026-04-07 12:17:20 -0500

    rename Inst to inst and adjust css

--- src/runtime/session.js
+++ src/runtime/session.js
@@ -2,10 +2,6 @@ export function logout() {
   sessionStorage.clear();
   localStorage.clear();
 
-  window.location.href =
-    'https://pragoptics.ciamlogin.com/pragoptics.onmicrosoft.com/oauth2/v2.0/logout' +
-    '?client_id=20a95281-4569-40f2-9b1d-74d5d5e03ca5' +
-    '&logout_hint=signedout' +
-    '&post_logout_redirect_uri=' +
-    encodeURIComponent('https://pragoptics.com/logout-complete');
+  // Return user to PragOptics in a signed-out state
+  window.location.replace('/');
 }
